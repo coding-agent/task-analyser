e19c4fb34b49d2edd0281f05cc1b8f85f781e90b
Marcius Silva <marciusdasilva51@gmail.com>
2023-12-19 20:02:12 +0000

    feat: introduce processes class for retrieving active processes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,18 @@
 package org.example;
 import javax.swing.JFrame;
-import org.example.system_data.Tasks;
 import org.example.system_data.Processes;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Processes.main(args);
+        Processes processes = new Processes();
+
+        ArrayList<String> processesList = processes.getProcesses();
+
         JFrame frame = new JFrame("Task Analyser");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
 
         frame.setVisible(true);
-
-        Tasks taskInfo = new Tasks();
-        taskInfo.getTasks();
-
     }
 }
--- src/main/java/org/example/system_data/Processes.java
+++ src/main/java/org/example/system_data/Processes.java
@@ -2,24 +2,31 @@ package org.example.system_data;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class Processes {
+    private ArrayList<String> processes;
 
-    public static void main(String[] args) {
+    public ArrayList<String> getProcesses() {
+        checkProcesses();
+        return this.processes;
+    }
+
+    private void checkProcesses () {
         try {
             Process process = new ProcessBuilder("tasklist").start();
 
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            ArrayList<String> arr = new ArrayList<>();
             String line;
 
             while ((line = reader.readLine()) != null) {
-                // Process each line (which contains information about a running process)
-                System.out.println(line);
+                arr.add(line);
             }
 
-            // Wait for the process to finish
             int exitCode = process.waitFor();
             System.out.println("Command exited with code " + exitCode);
+            this.processes = arr;
 
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
